13e3e9d7ca103e79546a937d5bf12ca41c47c8e6
JuneAekkalak <aekkalak.ju@gmail.com>
2022-12-25 20:20:12 +0700

    edit record detail

--- controller/recorddetail.js
+++ controller/recorddetail.js
@@ -1,4 +1,5 @@
 const RecordDetail = require('../model/recorddetail');
+const DailyRecord = require('../model/dailyrecord');
 
 // exports.getAllDailyRecord = (req, res, next) => {
 //     RecordDetail.findAll().then(recorddetail => {
@@ -12,21 +13,63 @@ const RecordDetail = require('../model/recorddetail');
 //         });
 //     });
 // }
+// async function getLatestId() {
+//     try {
+//         const [getDailyRecord] = await DailyRecord.getIdRecord();
+//         const daily = getDailyRecord;
+//         return daily[0].id;
+
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+// getLatestId().then((latestId) => {
+//     console.log(latestId);
+//     result = latestId ;
+//     console.log(result);
+//     // Use the latestId variable here
+// });
 
 exports.addRecordDetail = (req, res, next) => {
     const Symptom_id = req.body.Symptom_id;
-    const DailyRecord_id = req.body.DailyRecord_id;
-    console.log(Symptom_id);
-    Symptom_id.forEach(id => {
-        console.log(id);
-        const recorddetail = new RecordDetail(id, DailyRecord_id);
-        recorddetail.save().catch((error) => {
-            res.status(200).json({
-                "message": error,
-                "result": false
+    async function getLatestId() {
+        try {
+            const [getDailyRecord] = await DailyRecord.getIdRecord();
+            const daily = getDailyRecord;
+            return daily[0].id;
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    getLatestId().then((latestId) => {
+        console.log(latestId);
+        console.log(Symptom_id);
+
+        Symptom_id.forEach(id => {
+            console.log(id);
+            const recorddetail = new RecordDetail(id, latestId);
+            recorddetail.save().catch((error) => {
+                res.status(200).json({
+                    "message": error,
+                    "result": false
+                });
             });
         });
     });
+    // const DailyRecord_id = req.body.DailyRecord_id;
+    // console.log(Symptom_id);
+    // Symptom_id.forEach(id => {
+    //     console.log(id);
+    //     const recorddetail = new RecordDetail(id, DailyRecord_id);
+    //     recorddetail.save().catch((error) => {
+    //         res.status(200).json({
+    //             "message": error,
+    //             "result": false
+    //         });
+    //     });
+    // });
     res.send({ success: 'Values inserted successfully' });
 }
 
